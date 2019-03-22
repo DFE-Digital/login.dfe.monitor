26d0c43dee8def6800b0767309792e881e0a72c5
Simon Bull <simon.bull.84@googlemail.com>
2019-03-22 13:30:43 +0000

    Cache job counts

--- server/src/data/Store.js
+++ server/src/data/Store.js
@@ -2,6 +2,13 @@ class Store {
   constructor() {
     this.jobs = {
       categories: [],
+      typeCounts: [],
+      overallCounts: {
+        inactive: 0,
+        active: 0,
+        failed: 0,
+        completed: 0,
+      },
     };
   }
 
@@ -11,6 +18,29 @@ class Store {
 
   getJobCategories() {
     return this.jobs.categories.map(c => Object.assign({}, c));
+  }
+
+
+  setJobTypeCounts(typeCounts) {
+    console.info(`Set counts for type ${typeCounts.id}`);
+    const existing = this.jobs.typeCounts.find(t => t.id === typeCounts.id);
+    if (existing) {
+      Object.assign(existing, typeCounts);
+    } else {
+      this.jobs.typeCounts.push(typeCounts);
+    }
+  }
+
+  setJobOverallCounts(counts) {
+    console.info('set overall counts');
+    this.jobs.overallCounts = counts;
+  }
+
+  getJobCounts() {
+    return {
+      totals: Object.assign({}, this.jobs.overallCounts),
+      types: this.jobs.typeCounts.map(t => Object.assign({}, t)),
+    };
   }
 }
 
--- server/src/data/collectors/index.js
+++ server/src/data/collectors/index.js
@@ -1,10 +1,13 @@
 const jobCategoryCollector = require('./jobCategoryCollector');
+const jobCountCollector = require('./jobCountCollector');
 
 const start = (store) => {
   jobCategoryCollector.start(store);
+  jobCountCollector.start(store);
 };
 const stop = () => {
   jobCategoryCollector.stop();
+  jobCountCollector.stop();
 };
 
 module.exports = {

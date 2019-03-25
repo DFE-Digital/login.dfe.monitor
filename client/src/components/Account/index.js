21186a7da6cef71ee1d1340951dee3d2f401c033
Simon Bull <simon.bull.84@googlemail.com>
2019-03-25 09:36:32 +0000

    Display logged in user name in UI

--- client/src/components/Account/index.js
+++ client/src/components/Account/index.js
@@ -2,9 +2,21 @@ import React, { Component } from 'react';
 
 class Account extends Component {
   render() {
+    const cookies = window.document.cookie.split(';')
+      .filter(x => x)
+      .map((x) => {
+        const idx = x.indexOf('=');
+        return {
+          name: x.substr(0, idx),
+          value: x.substr(idx + 1),
+        };
+      });
+    const token = cookies.find(c => c.name === 'identity_details');
+    const id = JSON.parse(Buffer.from(token.value, 'base64').toString('utf8'));
+    
     return (
       <div className="logged-in-profile">
-        <div className="account-name">Some User</div>
+        <div className="account-name">{id.given_name} {id.family_name}</div>
         <div><a href="#">Sign out</a></div>
       </div>
     );
